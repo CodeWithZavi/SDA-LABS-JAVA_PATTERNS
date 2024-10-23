9535c4938ed188e3c6d9fe84c929787d39d2d009
CodeZavian <nomiemerge9901@gmail.com>
2024-10-23 07:19:31 +0500

    ////

--- Obderver Pattern/Subject.java
+++ Obderver Pattern/Subject.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Subject {
-	
    private List<Observer> observers = new ArrayList<Observer>();
    private int state;
 
@@ -15,17 +14,36 @@ public class Subject {
       notifyAllObservers();
    }
 
-   public void attach(Observer observer){
-      observers.add(observer);		
+   public void attach(Observer observer) {
+      observers.add(observer);
    }
-   public void detach(Observer observer){
-       observers.remove(observer);
+
+   public void detach(Observer observer) {
+      observers.remove(observer);
    }
-   public void notifyAllObservers(){
+
+   public void notifyAllObservers() {
       for (Observer observer : observers) {
-           if(observer.isActive){
-              observer.update(); 
-           }
+         if (observer.isActive) {
+            observer.update();
+         }
       }
-   } 	
+   }
+
+   public static void main(String[] args) {
+      Subject subject = new Subject();
+
+      new HexaObserver(subject);
+      OctalObserver octalObserver = new OctalObserver(subject);
+      new BinaryObserver(subject);
+
+      System.out.println("First state change: 15");
+      subject.setState(15);
+
+      // Deactivate OctalObserver
+      octalObserver.changeState(false);
+
+      System.out.println("Second state change: 50");
+      subject.setState(50);
+   }
 }
